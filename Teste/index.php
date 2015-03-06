1ba5e8cf5a54a0a3fc14edcaa691820d4e053f75
adriel <adriel@linux-936r>
2015-03-06 14:51:15 -0300

    Commit com com upload; tamanho fontes e layoutes definidos

---
+++ Teste/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta chdarset="utf-8">
+        <title>A Simple Page with CKEditor</title>
+        <!-- Make sure the path to CKEditor is correct. -->
+        <script src="ckeditor/ckeditor.js" type="text/javascript"></script>
+        </head>
+    <body>
+        <form>
+            <textarea  name="editor1" id="editor1" rows="10" cols="10">
+            </textarea>
+            <script>
+                // Replace the <textarea id="editor1"> with a CKEditor
+                // instance, using default configuration.
+                CKEDITOR.replace( 'editor1' );
+            </script>
+        </form>
+    </body>
+</html>
